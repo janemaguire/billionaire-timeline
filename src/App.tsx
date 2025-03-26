3ba383b2cdad76190ea75cab4ff8002c90642f1a
janemaguire <jane.m.maguire@gmail.com>
2025-03-26 11:12:01 +0000

    Adds radio buttons for earnings time

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import './App.css'
 function App() {
 
   function calculateYears(formData) {
-    const salary = formData.get("salary");
+    const earnings = formData.get("earnings");
+    const period = formData.get("period");
+    let salary;
+
+    period === "week" ? salary = earnings * 52 :
+    period === "month" ? salary = earnings * 12 : 
+    salary = earnings;
+
     const years = Math.round(1000000000 / salary).toLocaleString('en');
     alert(`'${years} years to go!'`);
   }
@@ -12,9 +19,23 @@ function App() {
     <>
       <h1>Billionaire timeline</h1>
       <p>How long until you earn a billion?</p>
-      <p>Enter your salary to find out</p>
+      <p>Enter your earnings to find out</p>
       <form action={calculateYears}>
-        <input name="salary" />
+
+        <input type="text" name="earnings" required/>
+        
+        <label>
+          <input type="radio" name="period" value="year" required/>
+          Yearly
+        </label>
+        <label>
+          <input type="radio" name="period" value="month" />
+          Monthly
+        </label>
+        <label>
+          <input type="radio" name="period" value="week" />
+          Weekly
+        </label>
         <button type="submit">Submit</button>
       </form>
     </>
