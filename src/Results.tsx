2df07eadda3f162e35352eb224859f2130e0805e
janemaguire <jane.m.maguire@gmail.com>
2025-04-12 17:50:21 +0100

    Adds earnings and period to results

--- src/Results.tsx
+++ src/Results.tsx
@@ -8,10 +8,10 @@ function Results ({result}) {
 
     return (
         <div>
-            <p>Mark it in your diary, you will have earnt a billion by the year {endYear}!</p>
-            <p>That's only {result.toLocaleString('en')} years away!</p>
-            <p>Or roughly {lifetimes} lifetimes.</p>
-            <p>Long enough to walk to the moon and back {moonYears} times.</p>
+            <p>Mark it in your diary, £{data.earnings.toLocaleString('en')} a {data.period} will earn you a billion by the year {data.endYear}!</p>
+            <p>That's only {data.yearsToGo.toLocaleString('en')} years away!</p>
+            <p>Or roughly {data.lifetimes.toLocaleString('en')} lifetimes.</p>
+            <p>Long enough to walk to the moon and back {data.moonYears.toLocaleString('en')} times.</p>
             <p>God speed!</p>
         </div>
     )
