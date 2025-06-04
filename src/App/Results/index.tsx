5342d75d33a65e60f0d7bd34d37555af9487d315
janemaguire <jane.m.maguire@gmail.com>
2025-06-04 18:42:21 +0100

    Changes superscript to avoid emoji on iPhone

--- src/App/Results/index.tsx
+++ src/App/Results/index.tsx
@@ -21,17 +21,18 @@ function Results ({data}) {
                     <p className="star-text" style={{opacity: 0, animationDelay: `${digits.length/2}s`}}>years to go!</p>
                 </div>
                 
-                <p>Earning £{data.earnings.toLocaleString('en')} a {data.period} will make you a billionaire by the year {data.endYear}.</p>
-                <p>That's only {data.yearsToGo.toLocaleString('en')} years away! Roughly {data.lifetimes.toLocaleString('en')} lifetimes<sup>*</sup>. 
-                    Or long enough to walk to the moon and back {data.moonYears.toLocaleString('en')} times<sup>&#10013;</sup>. But don't let that put you off.</p>
-                <p>God speed!</p>
+                <p>Earning £{data.earnings.toLocaleString('en')} a {data.period} will make you a billionaire by the year {data.endYear}. Only {data.yearsToGo.toLocaleString('en')} years away!</p>
+                <p>That's roughly {data.lifetimes.toLocaleString('en')} lifetimes<span>&#185;</span>. 
+                    Or long enough to walk to the moon and back {data.moonYears.toLocaleString('en')} times<span>&#178;</span>. Let the countdown commence!</p>
+                <p></p>
                 <a href="">Restart</a>
+                <p></p>
             </div>
 
             <div className="references">
                             
-                <p>* UK men and women, 2021-2023, <a href="https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/lifeexpectancies/bulletins/nationallifetablesunitedkingdom/2021to2023additionaldata" target='blank'><cite>NOS</cite></a></p>
-                <p><span>&#10013;</span><cite><a href="https://www.skyatnightmagazine.com/space-science/how-long-does-take-get-moon" target="blank"><cite>BBC Sky at Night</cite></a></cite></p>
+                <p>1. UK men and women, 2021-2023, <a href="https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/lifeexpectancies/bulletins/nationallifetablesunitedkingdom/2021to2023additionaldata" target='blank'><cite>NOS</cite></a></p>
+                <p>2. <cite><a href="https://www.skyatnightmagazine.com/space-science/how-long-does-take-get-moon" target="blank"><cite>BBC Sky at Night</cite></a></cite></p>
             
             </div>
         </>
